2df7d2e4e80880ca8c87d0263135ef4b4fb6def9
Heniteg <hempiteg@gmail.com>
2024-09-06 01:12:27 +0900

    moving the yolo weight to .gitignore

--- scripts/Running-Yolo/yoloBasics.py
+++ scripts/Running-Yolo/yoloBasics.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 import cv2
 
-model = YOLO('../../Yolo-Weights/yolov8n.pt')
+model = YOLO('../../Yolo-Weights/yolov8l.pt')
 results = model("images/image3.png", show=True)
 cv2.waitKey(0)
